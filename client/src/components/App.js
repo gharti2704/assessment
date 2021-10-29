c844cff61e6cda7a27fba2c1bf231357c149306b
Raju Gharti <63073106+gharti2704@users.noreply.github.com>
2021-10-29 10:07:59 -0400

    Create header and footer component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import Post from './Post';
+import Footer from './Footer';
+import Header from './Header';
+import { Container } from 'react-bootsrtap';
+
 const App = () => {
   return (
     <div className="App">
-      <Post />
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Post />
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 };
--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 const PostList = (props) => {
-  return (
-    <div className="card">
-      <div>{props.post.uiserId}</div>
-      <div>{props.post.id}</div>
-      <div>{props.post.title}</div>
-      <div>{props.post.body}</div>
-    </div>
-  );
+  // return (
+  //   <div className="card">
+  //     <div>{props.post.uiserId}</div>
+  //     <div>{props.post.id}</div>
+  //     <div>{props.post.title}</div>
+  //     <div>{props.post.body}</div>
+  //   </div>
+  // );
+
+  <Card style={{ width: '18rem' }}>
+    <Card.Body>
+      <Card.Title>{props.title}</Card.Title>
+      <Card.Text>{props.body}</Card.Text>
+      <Card.Link href="#">Card Link</Card.Link>
+      <Card.Link href="#">Another Link</Card.Link>
+    </Card.Body>
+  </Card>;
 };
 
 export default PostList;
