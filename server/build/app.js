f9c9d929c7dde3b0e5165e548a0fc85fb34c9856
Raju Gharti <63073106+gharti2704@users.noreply.github.com>
2021-10-29 06:56:34 -0400

    Implement posts endpoint

--- server/build/app.js
+++ server/build/app.js
@@ -4,8 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var routes_1 = require("./routes/routes");
 var app = express_1.default();
 app.use(express_1.default.json());
+app.use('/api', routes_1.router);
 var port = 8000;
 app.listen(port, function () {
     console.log("Server is running on port " + port);
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import {router} from './routes/routes';
 
 const app = express();
 
 app.use(express.json());
+app.use('/api', router);
 
 const port = 8000;
 
