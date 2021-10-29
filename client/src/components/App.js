00e2c898662ec8885369e73781c6c444ce69b876
Raju Gharti <63073106+gharti2704@users.noreply.github.com>
2021-10-29 09:21:15 -0400

    Initialize frontend

---
+++ client/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PostComponent from './PostComponent';
+const App = () => {
+  return (
+    <div className="App">
+      <PostComponent />
+    </div>
+  );
+};
+
+export default App;
